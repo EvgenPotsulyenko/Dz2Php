940c7ea400950b887a7992257df7a8b54dcdf021
Поцылуенко <83078431+EvgenPotsulyenko@users.noreply.github.com>
2023-03-13 14:17:57 +0300

    Add files via upload

---
+++ Dz3.php
@@ -0,0 +1,55 @@
+<?php
+$Month = 2;
+switch ($Month)
+{
+    case 1:
+        $Month = 31;
+        echo "В месяце - " .$Month;
+    break;
+    case 2:
+        $Month = 28;
+        echo "В месяце - " .$Month;
+        break;
+    case 3:
+        $Month = 31;
+        echo "В месяце - " .$Month;
+        break;
+    case 4:
+        $Month = 30;
+        echo "В месяце - " .$Month;
+        break;
+    case 5:
+        $Month = 31;
+        echo "В месяце - " .$Month;
+        break;
+    case 6:
+        $Month = 30;
+        echo "В месяце - " .$Month;
+        break;
+    case 7:
+        $Month = 31;
+        echo "В месяце - " .$Month;
+        break;
+    case 8:
+        $Month = 31;
+        echo "В месяце - " .$Month;
+        break;
+    case 9:
+        $Month = 30;
+        echo "В месяце - " .$Month;
+        break;
+    case 10:
+        $Month = 31;
+        echo "В месяце - " .$Month;
+        break;
+    case 11:
+        $Month = 30;
+        echo "В месяце - " .$Month;
+        break;
+    case 12:
+        $Month = 31;
+        echo "В месяце - " .$Month;
+        break;
+}
+
+?>
